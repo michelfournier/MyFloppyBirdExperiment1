13922bf3921e4228122b47012e57a8577f353ed4
michelfournier <michel.fournier.trad@gmail.com>
2024-04-05 15:10:57 +0200

    added note to to LogicScript

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; //to get the Text class
 
 public class LogicScript : MonoBehaviour
 {
